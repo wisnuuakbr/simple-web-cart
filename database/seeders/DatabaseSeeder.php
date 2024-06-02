ece7554da4af3a1a2232dd54b6054994dbe8370c
Adida Wisnu Akbara <wisnuaero3@gmail.com>
2024-06-02 10:08:26 +0700

    feat(base): init commit project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Wisnu',
+            'email' => 'wisnu@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('pass123'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="pt-5 my-5 text-muted text-center">
+    Created by <a href="https://github.com/wisnuuakbr">Wisnu Akbara</a> &middot; &copy; {{ date('M-Y') }}
+</footer>
